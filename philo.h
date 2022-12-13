cf077f84e775818808f934d5c3eea169304b0ec9
Chloé Y <80348069+YejLL@users.noreply.github.com>
2022-12-13 18:49:15 +0100

    Add files via upload

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: yejlee <yejlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 11:18:07 by yejlee            #+#    #+#             */
-/*   Updated: 2022/12/07 17:13:53 by yejlee           ###   ########.fr       */
+/*   Updated: 2022/12/13 18:38:34 by yejlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,22 @@
 
 typedef struct s_philo
 {
-	pthread_mutex_t	*left_f;
-	pthread_mutex_t	*right_f;
-	pthread_t		tid;
 	int				num;
 	int				status;
+	pthread_t		tid;
+	pthread_mutex_t	*left_f;
+	pthread_mutex_t	*right_f;
 	struct t_args	*arg;
-}		t_philo;
+}				t_philo;
 
-typedef struct	s_args
+typedef struct s_args
 {
 	int				num_philo;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				num_must_eat;
-	pthread_mutex_t	start;
+	int				start_time;
 	pthread_mutex_t	*fork;
 	pthread_mutex_t	messanger;
 	pthread_t		monitor;
@@ -47,5 +47,6 @@ typedef struct	s_args
 
 int		ft_atoi(const char *str);
 void	ft_putendl_fd(char *s, int fd);
+void	init_all(int ac, char *av[], t_args *arg);
 
 #endif
