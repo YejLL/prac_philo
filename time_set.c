d1c0e6e8febc25513bbb607e68f0d2dec5978aa9
Chloé Y <80348069+YejLL@users.noreply.github.com>
2022-12-23 13:33:50 +0100

    Add files via upload

--- time_set.c
+++ time_set.c
@@ -6,7 +6,7 @@
 /*   By: yejlee <yejlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 13:30:33 by yejlee            #+#    #+#             */
-/*   Updated: 2022/12/23 11:18:57 by yejlee           ###   ########.fr       */
+/*   Updated: 2022/12/23 13:33:35 by yejlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,4 @@ long long	get_time(void)
 		return (print_err("ERROR IN TIME"));
 	ms_time = ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 	return (ms_time);
-}
-
-long long	timed_now(struct timeval *start, struct timeval *end)
-{
-	long	diff;
-
-	diff = (end->tv_sec - start->tv_sec) * 1e3 + \
-				(end->tv_usec - start->tv_usec) / 1e3;
-	return (diff);
 }
